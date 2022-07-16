0c6e476443383ccc61e306f8e4f5d9a0fe6c074b
davikleber <davikleber@outlook.com>
2022-07-16 20:33:52 -0300

    Inicio projeto

---
+++ Back/src/Eventos.API/Controllers/EventosController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using Eventos.API.Models;
+
+namespace Eventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[] {
+                new Evento(){
+
+                EventoId = 1,
+                Tema = "Angular .NET 5 EF",
+                Local = "Fortaleza",
+                Lote = "Lote 1°",
+                QntPessoa = 5663,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "Foto1.png"
+            },
+
+             new Evento(){
+
+                EventoId = 2,
+                Tema = "EntityframeWork Core",
+                Local = "Recife",
+                Lote = "Lote 2°",
+                QntPessoa = 78963,
+                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                ImagemURL = "Foto2.png"
+            }            
+
+           };
+         
+        public EventosController()
+        {
+           
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+           return _evento;
+           
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetByid(int id)
+        {
+           return _evento.Where(evento=>evento.EventoId == id);;
+           
+        }
+
+        
+    }
+}
---
+++ Back/src/Eventos.API/Models/Evento.cs
@@ -0,0 +1,14 @@
+namespace Eventos.API.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QntPessoa { get; set; }
+        public string Lote { get; set; }
+        public string ImagemURL { get; set; }
+
+    }
+}
